47eacc633a06e88e190b421c23275742e6b90c6d
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2026-06-13 12:44:58 +0530

    deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,11 @@ import "dotenv/config"
 import cors from "cors"
 import { connectDB } from "./lib/db.js"
 import { clerkMiddleware } from "@clerk/express"
-
+import fs from "fs";
+import path from "path";
 
 const PORT = process.env.PORT || 3000
+const publicDir = path.join(process.cwd(), "public");
 const app = express()
 app.use(express.json());
 app.use(clerkMiddleware())
@@ -16,7 +18,20 @@ app.get('/',(req,res)=>{
     
 })
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server running on port ${PORT}`)
-})
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
+
+  if (process.env.NODE_ENV === "production") job.start();
+});
