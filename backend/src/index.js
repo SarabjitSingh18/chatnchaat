2958f63e5941ce8c6ffb5505c6d9f66b63fcf908
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2026-06-13 12:55:07 +0530

    bug fixed in index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,9 +29,9 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on PORT:", PORT);
 
-  if (process.env.NODE_ENV === "production") job.start();
+
 });
