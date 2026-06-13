99ca3d46410999137fb2cca5f482174f9b643360
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2026-06-13 12:28:22 +0530

    deploy test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,22 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import { connectDB } from "./lib/db.js"
+import { clerkMiddleware } from "@clerk/express"
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json());
-app.use(cors({ origin:'*' }));
+app.use(clerkMiddleware())
+app.use(cors({ origin:process.env.FRONTEND_URL,credentials:true }));
 
 app.get('/',(req,res)=>{
-    res.send("Hello World")
+    res.send(" Server is working good!!!!")
     
 })
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running on port ${PORT}`)
 })
