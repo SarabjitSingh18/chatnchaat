ff1a876f244fd9d1a1adef044b08a2ecfbfa1e97
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2026-06-13 23:28:16 +0530

    backend completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,40 @@
-import express from "express"
-import "dotenv/config"
-import cors from "cors"
-import { connectDB } from "./lib/db.js"
-import { clerkMiddleware } from "@clerk/express"
+import express from "express";
+import cors from "cors";
+
+import "dotenv/config";
+
 import fs from "fs";
 import path from "path";
-import job from "./lib/cron.js"
-import clerkWebhook from "./webhooks/clerk.webhook.js"
 
-const app = express()
+import { clerkMiddleware } from "@clerk/express";
 
-const PORT = process.env.PORT || 3000
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
 const publicDir = path.join(process.cwd(), "public");
+
+// it's important that you don't parse the webhook event data, it should be in the raw format
 app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
-app.use(clerkMiddleware())
-app.use(cors({ origin:process.env.FRONTEND_URL,credentials:true }));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
@@ -33,9 +46,9 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on PORT:", PORT);
 
- if (process.env.NODE_ENV === "production") job.start();
+  if (process.env.NODE_ENV === "production") job.start();
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js"
+import { checkAuth } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-router.get("/check",protectRoute)
+router.get("/check",protectRoute,checkAuth)
 
 export default router   
