3e9dc8656f6495d38f807497b29d2314ded73c2a
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2026-06-18 11:45:24 +0530

    coding part done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,27 @@ import { Navigate, Route, Routes } from 'react-router'
 import ChatPage from "./pages/ChatPage"
 import AuthPage from "./pages/AuthPage"
 import { useAuth } from '@clerk/react'
+import PageLoader from "./components/PageLoader"
+import { useEffect } from "react"
+import { useAuthStore } from "./store/useAuthStore"
+
+import { Toaster } from "react-hot-toast";
 const App = () => {
    const { isSignedIn, isLoaded } = useAuth();
+
+    
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+    useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
   return (
 
     <ThemeProvider>
@@ -19,6 +38,7 @@ const App = () => {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
   )
